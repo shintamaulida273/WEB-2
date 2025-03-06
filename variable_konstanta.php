0094313900fcd7af537aa0038eec275ced395bbc
shintamaulida273 <0110124201@student.nurulfikri.ac.id>
2025-03-06 13:08:11 +0700

    praktikum 3

--- variable_konstanta.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("PHI", 3.14);
-$jari_jari = 10;
-$luas = PHI * $jari_jari * $jari_jari;
-
-echo "Nilai PHI adalah " . PHI;
-echo "<br/>Luas lingkaran dari jari-jari " . $jari_jari . " adalah " . $luas;
-?>
