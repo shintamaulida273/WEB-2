ae3d95cc13bdc8073c59cad00c28d90d0f79b35f
shintamaulida273 <0110124201@student.nurulfikri.ac.id>
2025-03-13 10:34:04 +0700

    praktikum 4

---
+++ class_animal.php
@@ -0,0 +1,55 @@
+<?php
+
+class Animal {
+    public $animals;
+
+    public function __construct($ar_animal)
+    {
+        $this->animals = $ar_animal;
+    }
+
+    public function index(){
+        foreach ($this->animals as $animal){
+            echo "- $animal <br/>";
+        }
+    }
+
+    public function store($animal){
+        $this->animals[] = $animal;
+    }
+
+    public function update($index, $animal){
+        $this->animals[$index] = $animal;
+    }
+
+    public function destroy($index){
+        unset($this->animals[$index]);
+    }
+}
+
+#membuat object
+#kirimkan data array ke dalam constructor
+$animal = new Animal(["Ayam", "Ikan"]);
+
+#methode index
+echo "Index - Menampilkan seluruh hewan <br/>";
+$animal->index();
+echo "<br/>";
+
+#methode store
+echo "Store - Menambahkan hewan baru (burung) <br/>";
+$animal->store("Burung");
+$animal->index();
+echo "<br/>";
+
+#methode update
+echo "Update - Mengupdate hewan <br/>";
+$animal->update(0, "Kucing Anggora");
+$animal->index();
+echo "<br/>";
+
+#methode destroy
+echo "Destroy - Menghapus hewan <br/>";
+$animal->destroy(1);
+$animal->index();
+echo "<br/>";
